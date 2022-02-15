03482b3306afe9d6b7122c8b01b8148ffa457f0d
Usu2 Peculocus <usu2@example.com>
2022-02-15 23:41:52 +0100

    Mensaje de Validacion

---
+++ GitPrueba1/src/cambio1/Cambio1.java
@@ -0,0 +1,19 @@
+package cambio1;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Pruebas Git. Clase Cambio1");
+
+	}
+
+}
+class GitPrueba1{
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo java-git");
+
+	}
+
+}
+
